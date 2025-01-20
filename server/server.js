77c7d784563bf4215d58af561cd2cd2cc94f16cb
SonalKhare <sonalkhare.sk215@gmail.com>
2025-01-20 21:58:27 +1100

    files commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import cors from 'cors';
+import express from 'express';
+import { authMiddleware, handleLogin } from './auth.js';
+
+const PORT = 9000;
+
+const app = express();
+app.use(cors(), express.json(), authMiddleware);
+app.use(cors(), express.json());
+
+app.post('/login', handleLogin);
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on ${PORT}`);
+});
