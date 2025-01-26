37952d9bc4ccbe8991f32de8cdca1c5c446c14d0
SonalKhare <sonalkhare.sk215@gmail.com>
2025-01-26 12:37:19 +1100

    server dataloader class for caching files commit

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import JobList from '../components/JobList';
 // import { jobs } from '../lib/fake-data';
 // import { getJobs } from '../lib/graphql/queries';
 import { useJobs } from '../lib/graphql/hooks';
+import PaginationBar from '../components/PaginationBar';
 
-const JOBS_PER_PAGE = 20;
+const JOBS_PER_PAGE = 5;
 
 const HomePage = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -26,17 +27,17 @@ const HomePage = () => {
   if (error) {
     return <div className="has-text-danger">Data unavailable</div>;
   }
+  const totalPages = Math.ceil(jobs.totalCount / JOBS_PER_PAGE);
   return (
     <div>
       <h1 className="title">Job Board</h1>
-      <div>
-        <button onClick={() => setCurrentPage(currentPage - 1)}>
-          Previous
-        </button>
-        <span> {currentPage} </span>
-        <button onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
-      </div>
-      <JobList jobs={jobs} />
+
+      <PaginationBar
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
+      <JobList jobs={jobs.items} />
     </div>
   );
 };
